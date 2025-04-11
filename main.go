8abdbf71d69922ea131a6e1e277b401d2dd35d40
DryHop2 <tyler.savon.smith@gmail.com>
2025-04-11 08:37:17 -0600

    separate code into command and repl files per Go convention

--- main.go
+++ main.go
@@ -1,35 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
-
-func cleanInput(text string) []string {
-	loweredWords := strings.Fields(strings.ToLower(text))
-	return loweredWords
-}
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		if !scanner.Scan() {
-			break
-		}
-
-		input := scanner.Text()
-		words := cleanInput(input)
-
-		if len(words) == 0 {
-			continue
-		}
-		fmt.Println("Your command was:", words[0])
-
-		if err := scanner.Err(); err != nil {
-			fmt.Fprintln(os.Stderr, "reading standard input:", err)
-		}
-	}
+	startRepl()
 }
